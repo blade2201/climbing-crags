fe4a3b0ebd30964f12b056a2e3380402e4a8e286
Samuele Venier <samuele.venier@ensoul.it>
2022-07-27 17:10:30 +0200

    added some styles to the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import Layout from '../components/layout';
 
 export default function Home() {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/${searchTerm}`);
+  };
+
   return (
     <>
       <div
-        className="mt-12 h-[80vh] flex items-center justify-center flex-col clip-image bg-cover"
+        className="mt-12 h-[80vh] flex items-center justify-center flex-col clip-image bg-cover mb-80 bg-[-15rem_center] md:bg-center"
         style={{ backgroundImage: 'url("/home.jpg")' }}
       >
-        <h1 className="text-white-true font-semibold text-9xl">Climbing Crags</h1>
-        <form action="submit">
-          <input type="text" />
-          <button type="submit">send</button>
+        <h1 className="text-white-true font-semibold md:text-9xl text-4xl">Climbing Crags</h1>
+        <form
+          action="submit"
+          className="md:mt-20 mt-12 md:w-1/2 w-full flex md:gap-x-6 gap-x-2 px-4"
+          onSubmit={handleOnSubmit}
+        >
+          <input
+            type="text"
+            placeholder="Type to search..."
+            className="w-full"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
+          />
+          <button className="button" type="submit">
+            Search
+          </button>
         </form>
       </div>
     </>
