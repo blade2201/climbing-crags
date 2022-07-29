3fc4e99d4513b1d1d3be00f3cc820ac58ddfc0a5
Samuele Venier <samuele.venier@ensoul.it>
2022-07-29 08:39:47 +0200

    add: new rating component and changed star.svg filename

---
+++ components/ui/Rating.js
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+
+function Rating({ rating }) {
+  return (
+    <div className="px-4 py-2 bg-[#474747] rounded-2xl w-max flex items-center gap-x-1">
+      <Image
+        src={`/star-${rating > 1 ? 'full' : rating > 0.5 ? 'half' : 'empty'}.svg`}
+        alt="star svg"
+        width={32}
+        height={32}
+      />
+      <Image
+        src={`/star-${rating > 2 ? 'full' : rating > 1.5 ? 'half' : 'empty'}.svg`}
+        alt="star svg"
+        width={32}
+        height={32}
+      />
+      <Image
+        src={`/star-${rating > 3 ? 'full' : rating > 2.5 ? 'half' : 'empty'}.svg`}
+        alt="star svg"
+        width={32}
+        height={32}
+      />
+      <Image
+        src={`/star-${rating > 4 ? 'full' : rating > 3.5 ? 'half' : 'empty'}.svg`}
+        alt="star svg"
+        width={32}
+        height={32}
+      />
+      <Image
+        src={`/star-${rating > 5 ? 'full' : rating > 4.5 ? 'half' : 'empty'}.svg`}
+        alt="star svg"
+        width={32}
+        height={32}
+      />
+    </div>
+  );
+}
+
+export default Rating;
--- components/ui/Tag.js
+++ components/ui/Tag.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 function Tag({ text, star }) {
   return (
     <div className="px-2 py-1 font-semibold border border-primary-600 rounded-4xl text-sm min-w-[30px] flex justify-center gap-x-1">
-      {text} {star && <Image src="/star.svg" alt="star svg" width={15} height={14} />}
+      {text} {star && <Image src="/star-small.svg" alt="star svg" width={15} height={14} />}
     </div>
   );
 }
