42972b620601184f6371704f722f4403d4e3fe75
Samuele Venier <samuele.venier@ensoul.it>
2022-08-01 12:58:56 +0200

    Api to associate images to route

---
+++ pages/api/routes/index.js
@@ -0,0 +1,29 @@
+import clientPromise from '../../../utils/mongodb';
+
+//TODO Add secret key to prevent unauthorized access
+export default async function handler(req, res) {
+  const body = req.body;
+  if (req.method === 'PUT') {
+    if (!body) {
+      return res.status(400).json({ error: 'Bad request (No body)' });
+    }
+    if (!body.id || !body.imageSrc || !body.path) {
+      return res.status(400).json({ error: 'Bad request (Missing fields)' });
+    }
+    try {
+      const client = await clientPromise;
+      const db = client.db('Climbing-crags');
+      const routesCollection = db.collection('routes');
+      const routesCursor = await routesCollection.findOneAndUpdate(
+        { id: body.id },
+        { $push: { images: body.imageSrc } },
+      );
+      return res.status(200).json(routesCursor);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  return res.status(400).json({ error: 'Bad request (Wrong method)' });
+}
