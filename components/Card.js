b9abc083974232776b4db94ceddf056d2a99fce7
Samuele Venier <samuele.venier@ensoul.it>
2022-08-02 21:27:26 +0200

    Loading low quality images from cloudinary while the main images load

--- components/Card.js
+++ components/Card.js
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { calcRoutesAndDifficulty } from '../utils/infoCalc';
+import { buildUrl } from 'cloudinary-build-url';
 import Tag from './ui/Tag';
 function Card({ item, type }) {
   const [routesCount, setRoutesCount] = useState('');
   const [difficulty, setDifficulty] = useState('');
   const [rating, setRating] = useState(0);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState('');
 
   useEffect(() => {
     if (item) {
@@ -27,13 +29,42 @@ function Card({ item, type }) {
           break;
       }
     }
+
+    if (item.images.length) {
+      const imageId = item.images[0][0].id;
+      const url = buildUrl(imageId, {
+        cloud: {
+          cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_ID,
+        },
+        transformations: {
+          // effect: {
+          //   name: 'pixelate',
+          //   value: 100,
+          // },
+          effect: 'blur:1000',
+          quality: 1,
+        },
+      });
+      setImagePreviewUrl(url);
+    }
   }, [item, type]);
 
   return (
     <div className="col-span-1 bg-dark-card rounded-4xl p-5 shadow-8 relative">
-      <div className="rounded-3xl overflow-hidden aspect-video relative">
+      <div
+        className="rounded-3xl overflow-hidden aspect-video relative bg-cover bg-center"
+        style={{
+          backgroundImage: imagePreviewUrl
+            ? `url(${imagePreviewUrl})`
+            : 'url("https://res.cloudinary.com/blade2201/image/upload/c_crop,e_blur:400,h_949,q_30,w_1920/v1659337484/routes/wd5qupjyrgkmtwcuhoe9.jpg")',
+        }}
+      >
         <Image
-          src={item.images && item.images.length ? item.images[0][0].src : '/home.jpg'}
+          src={
+            item.images && item.images.length
+              ? item.images[0][0].src
+              : 'https://res.cloudinary.com/blade2201/image/upload/c_crop,h_949,w_1920/v1659337484/routes/wd5qupjyrgkmtwcuhoe9.jpg'
+          }
           alt="card-image"
           layout="fill"
           objectFit="cover"
