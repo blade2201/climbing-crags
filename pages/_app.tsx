78645d351cef1fe0acaa229f8fbe531184681f7f
hudsonbay97@gmail.com <hudsonbay97@gmail.com>
2022-08-05 10:55:19 +0200

    Add TS _app-tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,22 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import React from 'react';
+import * as React from 'react';
+import type { NextComponentType } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+type CustomAppProps = AppProps & {
+  Component: CustomComponent;
+};
+
+type CustomComponent = NextComponentType & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: CustomAppProps) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
