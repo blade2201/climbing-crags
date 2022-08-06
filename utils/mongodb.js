f62e673ec39ebdffe4291e69180a5006d8bc1baf
Matthew D'Agostino <mattdags982@gmail.com>
2022-08-06 11:31:23 +0200

    typed ListSection and most of Home

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 const options = {};
@@ -7,7 +7,7 @@ let client;
 let clientPromise;
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+  throw new Error("Please add your Mongo URI to .env.local");
 }
 
 // In development mode, use a global variable so that the value
