8f0022250a38f24ec3684b1f3c23281f1c2f2d83
Matthew D'Agostino <mattdags982@gmail.com>
2022-08-06 18:02:44 +0200

    Typed api-comment

--- pages/route/[name].tsx
+++ pages/route/[name].tsx
@@ -1,19 +1,19 @@
-import Layout from '../../components/Layout';
-import Image from 'next/image';
-import clientPromise from '../../utils/mongodb';
-import { getFrGrade } from '../../utils/infoCalc';
-import Rating from '../../components/ui/Rating';
-import Link from 'next/link';
-import CommentSection from '../../components/CommentSection';
-import { useState } from 'react';
-import Close from '../../public/close.svg';
-import { useRouter } from 'next/router';
-import { useSession, signIn } from 'next-auth/react';
-import * as React from 'react';
-import { CommentType } from '../../types/Comment';
-import { Route } from '../../types/mattTypes';
-import { Collection, MongoClient } from 'mongodb';
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
+import Layout from "../../components/Layout";
+import Image from "next/image";
+import clientPromise from "../../utils/mongodb";
+import { getFrGrade } from "../../utils/infoCalc";
+import Rating from "../../components/ui/Rating";
+import Link from "next/link";
+import CommentSection from "../../components/CommentSection";
+import { useState } from "react";
+import Close from "../../public/close.svg";
+import { useRouter } from "next/router";
+import { useSession, signIn } from "next-auth/react";
+import * as React from "react";
+import { CommentType } from "../../types/Comment";
+import { Route } from "../../types/mattTypes";
+import { Collection, MongoClient } from "mongodb";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 
 export default function RoutePage({
   route,
@@ -24,13 +24,13 @@ export default function RoutePage({
 }) {
   const { data: session } = useSession();
   const router = useRouter();
-  const [imageSrc, setImageSrc] = useState('');
+  const [imageSrc, setImageSrc] = useState("");
   const [uploadImage, setUploadImage] = useState(false);
   const [uploadingImage, setUploadingImage] = useState(false);
   const [routeImage, setRouteImage] = useState(
     route.images && route.images.length
       ? route.images[0].src
-      : 'https://res.cloudinary.com/blade2201/image/upload/c_crop,h_949,w_1920/v1659337484/routes/wd5qupjyrgkmtwcuhoe9.jpg'
+      : "https://res.cloudinary.com/blade2201/image/upload/c_crop,h_949,w_1920/v1659337484/routes/wd5qupjyrgkmtwcuhoe9.jpg"
   );
   const [formDisabled, setFormDisabled] = useState(false);
   const sum = comments.reduce((acc, cur) => acc + cur.rating, 0);
@@ -47,7 +47,7 @@ export default function RoutePage({
   };
 
   function showUploadForm() {
-    setImageSrc('');
+    setImageSrc("");
     setUploadImage((prevState) => !prevState);
   }
 
@@ -57,7 +57,7 @@ export default function RoutePage({
       const cloudinaryId = process.env.NEXT_PUBLIC_CLOUDINARY_ID;
       const form = e.currentTarget;
       const fileInput: any = Array.from(form.elements).find(
-        ({ name }: any) => name === 'file'
+        ({ name }: any) => name === "file"
       );
       const formData = new FormData();
       if (fileInput.files[0].size > 1048576) {
@@ -68,26 +68,27 @@ export default function RoutePage({
         setFormDisabled(false);
       }
       for (const file of fileInput.files) {
-        formData.append('file', file);
+        formData.append("file", file);
       }
-      formData.append('upload_preset', 'climbing-crag');
+      formData.append("upload_preset", "climbing-crag");
       const data = await fetch(
         `https://api.cloudinary.com/v1_1/${cloudinaryId}/upload`,
         {
-          method: 'POST',
+          method: "POST",
           body: formData,
         }
       );
       const json = await data.json();
-      setImageSrc('');
+
+      setImageSrc("");
       setUploadingImage(false);
       setUploadImage(false);
       setRouteImage(json.secure_url);
       e.target.reset();
       const response = await fetch(`/api/routes/`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           imageSrc: json.secure_url,
@@ -103,12 +104,12 @@ export default function RoutePage({
   };
   return (
     <>
-      <section className='px-4 md:px-36 pt-12 relative min-h-[70vh]'>
+      <section className="px-4 md:px-36 pt-12 relative min-h-[70vh]">
         <div>
-          <h1 className='text-3xl md:text-6xl font-semibold md:font-bold mb-4 text-white-high capitalize'>
+          <h1 className="text-3xl md:text-6xl font-semibold md:font-bold mb-4 text-white-high capitalize">
             {route.name}
           </h1>
-          <h4 className='md:text-4xl text-white-high'>
+          <h4 className="md:text-4xl text-white-high">
             <Link href={`/sector/${route.sector_id}`}>
               <a>{route.sector}, </a>
             </Link>
@@ -116,29 +117,29 @@ export default function RoutePage({
               <a>{route.crag}</a>
             </Link>
           </h4>
-          <div className='hidden md:block'>
-            <div className='pt-20 text-2xl text-white flex items-center gap-x-4'>
+          <div className="hidden md:block">
+            <div className="pt-20 text-2xl text-white flex items-center gap-x-4">
               Grade:
-              <div className='border rounded-full flex items-center aspect-square p-4 border-primary-600'>
+              <div className="border rounded-full flex items-center aspect-square p-4 border-primary-600">
                 {getFrGrade(route.grade_id)}
               </div>
             </div>
-            <div className='pt-8 text-2xl text-white flex items-center gap-x-4'>
-              Rating:{' '}
+            <div className="pt-8 text-2xl text-white flex items-center gap-x-4">
+              Rating:{" "}
               <Rating rating={rating ? rating : parseInt(route.rating)} />
             </div>
-            <label className='relative group'>
+            <label className="relative group">
               <button
                 disabled={!session}
-                className='button mt-10'
+                className="button mt-10"
                 onClick={showUploadForm}
               >
                 + add your own image
               </button>
               {!session ? (
                 <button
-                  onClick={() => signIn('google')}
-                  className='absolute -top-[1.3rem] w-full flex justify-center left-0 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 button'
+                  onClick={() => signIn("google")}
+                  className="absolute -top-[1.3rem] w-full flex justify-center left-0 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 button"
                 >
                   Log in to upload
                 </button>
@@ -148,34 +149,34 @@ export default function RoutePage({
             </label>
           </div>
         </div>
-        <div className='absolute w-[150%] h-3/5 top-[30%] -left-32 md:top-10 md:left-[35%] md:w-[70%] md:h-full -rotate-2'>
+        <div className="absolute w-[150%] h-3/5 top-[30%] -left-32 md:top-10 md:left-[35%] md:w-[70%] md:h-full -rotate-2">
           <Image
-            className='rounded-4xl'
+            className="rounded-4xl"
             src={routeImage}
-            alt='route image'
-            layout='fill'
-            objectFit='cover'
+            alt="route image"
+            layout="fill"
+            objectFit="cover"
             priority={true}
           />
         </div>
       </section>
-      <section className='md:hidden px-4  mb-10'>
-        <div className='text-xl text-white flex items-center gap-x-4'>
+      <section className="md:hidden px-4  mb-10">
+        <div className="text-xl text-white flex items-center gap-x-4">
           Grade:
-          <div className='border rounded-full aspect-square p-2 border-primary-600'>
+          <div className="border rounded-full aspect-square p-2 border-primary-600">
             {getFrGrade(route.grade_id)}
           </div>
         </div>
-        <label className='relative group'>
+        <label className="relative group">
           <button
             disabled={!session}
-            className='button mt-6'
+            className="button mt-6"
             onClick={showUploadForm}
           >
             + add your own image
           </button>
           {!session ? (
-            <button className='absolute -top-[1.3rem] w-full flex justify-center left-0 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 button'>
+            <button className="absolute -top-[1.3rem] w-full flex justify-center left-0 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-200 button">
               Log in to upload
             </button>
           ) : (
@@ -185,54 +186,54 @@ export default function RoutePage({
       </section>
       <CommentSection comments={comments} />
       <div
-        id='modal'
+        id="modal"
         className={`flex items-center justify-center fixed top-0 left-0 w-screen h-screen bg-dark-60 z-50 px-5 md:px-0 ${
-          uploadImage ? '' : 'hidden'
+          uploadImage ? "" : "hidden"
         }`}
       >
         <form
-          className='bg-dark-card rounded-4xl p-8 shadow-8'
-          method='post'
+          className="bg-dark-card rounded-4xl p-8 shadow-8"
+          method="post"
           onChange={handleChange}
           onSubmit={handleSubmit}
         >
-          <div className='pb-6 text-2xl font-semibold flex justify-between items-center'>
+          <div className="pb-6 text-2xl font-semibold flex justify-between items-center">
             <h5>Upload your picture </h5>
-            <Close className='w-6 cursor-pointer' onClick={showUploadForm} />
+            <Close className="w-6 cursor-pointer" onClick={showUploadForm} />
           </div>
-          <p className='pl-1 text-sm pb-1'>Only upload horizontal images</p>
+          <p className="pl-1 text-sm pb-1">Only upload horizontal images</p>
           <input
-            className='file:bg-primary-600 w-full file:text-white-true file:border-0 rounded-4xl border border-primary-600 file:p-2 file:mr-2 cursor-pointer'
-            type='file'
-            name='file'
+            className="file:bg-primary-600 w-full file:text-white-true file:border-0 rounded-4xl border border-primary-600 file:p-2 file:mr-2 cursor-pointer"
+            type="file"
+            name="file"
           />
           {formDisabled && (
-            <p className='pl-1 text-xs pt-1 text-red-500'>
+            <p className="pl-1 text-xs pt-1 text-red-500">
               file is too big: max-size 1Mb
             </p>
           )}
-          <div className='aspect-video relative my-10 bg-white flex items-center justify-center rounded-2xl'>
+          <div className="aspect-video relative my-10 bg-white flex items-center justify-center rounded-2xl">
             {imageSrc ? (
               <Image
-                className='object-cover'
+                className="object-cover"
                 src={imageSrc}
-                layout='fill'
-                alt=''
+                layout="fill"
+                alt=""
               />
             ) : (
-              'Image Preview'
+              "Image Preview"
             )}
           </div>
-          <div className='flex items-center gap-x-5'>
+          <div className="flex items-center gap-x-5">
             <button
               disabled={!imageSrc || formDisabled}
-              className='button'
-              type='submit'
+              className="button"
+              type="submit"
             >
-              {uploadingImage ? 'uploading ' : 'Upload'}
+              {uploadingImage ? "uploading " : "Upload"}
             </button>
             {uploadingImage && (
-              <div className='spinner md:w-10 md:h-10 w-6 h-6 rounded-full'></div>
+              <div className="spinner md:w-10 md:h-10 w-6 h-6 rounded-full"></div>
             )}
           </div>
         </form>
@@ -248,8 +249,8 @@ RoutePage.getLayout = function getLayout(page) {
 // this preloads all the possible paths for the crag page
 export async function getStaticPaths() {
   const client = await clientPromise;
-  const db = client.db('Climbing-crags');
-  const routesCollection = db.collection('routes');
+  const db = client.db("Climbing-crags");
+  const routesCollection = db.collection("routes");
   const routesCursor = await routesCollection.find({});
   const routes: { id: string }[] = await routesCursor
     .map((route) => {
@@ -271,8 +272,8 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   let client: MongoClient;
   try {
     client = await clientPromise;
-    const db = client.db('Climbing-crags');
-    const routesCollection: Collection<Route> = db.collection('routes');
+    const db = client.db("Climbing-crags");
+    const routesCollection: Collection<Route> = db.collection("routes");
     const routesCursor = await routesCollection.find({
       id: ctx?.params?.name,
     });
@@ -282,7 +283,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
       })
       .toArray();
     const commentsCollection: Collection<CommentType> =
-      db.collection('comments');
+      db.collection("comments");
     const commentsCursor = await commentsCollection.find({
       path: `/route/${ctx?.params?.name}`,
     });
