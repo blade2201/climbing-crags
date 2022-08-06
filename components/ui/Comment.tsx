eddf465632f805857c4dbb1f35130e7419c8bc59
Kevin <kelott@gmx.de>
2022-08-06 12:18:22 +0200

    finish TS conversion of components/ui folder

--- components/ui/Comment.tsx
+++ components/ui/Comment.tsx
@@ -1,6 +1,28 @@
+import React from 'react';
 import Chevron from '../../public/chevron.svg';
 import Rating from './Rating';
-export default function Comment({ comment, onClick, user }) {
+
+type CommentType = {
+  _id: string;
+  title: string;
+  comment: string;
+  rating: number;
+  path: string;
+  user: string;
+  comment_rating: number;
+  id: string;
+  votes: {
+    [key: string]: number;
+  };
+};
+
+type CommentProps = {
+  comment: CommentType;
+  onClick: (upvote: boolean) => void;
+  user: string;
+};
+
+export default function Comment({ comment, onClick, user }: CommentProps) {
   return (
     <div className="bg-dark-card p-4 md:p-8 rounded-4xl flex items-center shadow-8 gap-x-8 mb-6">
       <div className="gap-y-4 flex flex-col justify-center">
@@ -38,7 +60,9 @@ export default function Comment({ comment, onClick, user }) {
       </div>
       <div className="flex-grow">
         <div className="flex justify-between min-w-full items-start mb-3 ">
-          <h3 className="text-2xl font-bold text-white-high">{comment.title}</h3>
+          <h3 className="text-2xl font-bold text-white-high">
+            {comment.title}
+          </h3>
           <div>
             {comment.user !== 'default' ? (
               <div className="text-right pr-4">{comment.user}</div>
--- components/ui/InfoCard.tsx
+++ components/ui/InfoCard.tsx
@@ -1,17 +1,24 @@
 import Rating from './Rating';
 
-export default function InfoCard({ rating, routes, difficulties, classes, type }) {
+type InfoCardProps = {
+  rating: number;
+  routes: number;
+  difficulties: string;
+  classes: string;
+  type: string;
+};
+
+export default function InfoCard({ rating, routes, difficulties, classes, type }: InfoCardProps) {
   return (
     <div className={'md:p-8 p-4 rounded-2xl md:rounded-4xl bg-dark-card shadow-8 w-max ' + classes}>
       <p className="md:text-2xl">
-        N° {type === 'crag' ? 'Sectors' : 'Routes'}:{' '}
-        <span className="font-bold text-primary-400">{routes}</span>
+        N° {type === 'crag' ? 'Sectors' : 'Routes'}: <span className="font-bold text-primary-400">{routes}</span>
       </p>
       <p className="md:text-2xl md:pb-10 pb-6">
         Grades: <span className="font-bold text-primary-400">{difficulties}</span>
       </p>
       <p className="md:text-2xl pb-2">People&apos;s rating:</p>
-      <Rating rating={rating} />
+      <Rating rating={rating} notBoxed={false} className={''} />
     </div>
   );
 }
