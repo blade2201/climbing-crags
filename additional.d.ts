51557622a25664aeb0b67fddb7de12d4d9c27c3c
Kevin <kelott@gmx.de>
2022-08-06 20:24:32 +0200

    convert _app.js to ts, modify type declarations

--- additional.d.ts
+++ additional.d.ts
@@ -1,3 +1,7 @@
+import { NextPage } from 'next';
+import { AppProps } from 'next/app';
+import React, { ReactElement, ReactNode } from 'react';
+
 export type CommentsType = {
   _id: string;
   title: string;
@@ -13,16 +17,16 @@ export type CommentsType = {
 };
 
 export type RoutesType = {
-  _id: string;
+  _id?: string;
   grade_id: number;
-  sector_id: number;
+  sector_id?: number;
   crag_id: number;
   name: string;
   rating: number;
   id: string;
-  sector: string;
-  crag: string;
-  images: [
+  sector?: string;
+  crag?: string;
+  images?: [
     {
       src: string;
       id: string;
@@ -30,4 +34,41 @@ export type RoutesType = {
   ];
   comments?: CommentsType[];
   avgRating?: number;
+};
+
+export type SectorsType = {
+  sector: string;
+  _id: string;
+  crag: string;
+  country: string;
+  crag_id: number;
+  images: any[];
+  routes: RoutesType[];
+  sector: string;
+  sector_id: number;
+};
+
+export type CragsType = {
+  country: string;
+  crag: string;
+  images: any[];
+  sectors: SectorsType[];
+  _id: string;
+};
+
+export type GradesType = {
+  id: number;
+  fra_routes: string;
+  usa_routes: string;
+};
+
+export type ReactNodeWithProps = React.ReactNode & {
+  props: {
+    grades?: GradesType[];
+    grade?: GradesType;
+    crags?: CragsType[];
+    crag?: CragsType;
+    sectors?: SectorsType[];
+    sector?: SectorsType;
+  };
 };
