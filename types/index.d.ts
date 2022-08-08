49c6eff7f408b2f0102d6e87aaebb2d39f5ffe17
Andreas Asséhn <andreas.assehn@gmail.com>
2022-08-08 09:04:23 +0200

    get it

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,14 @@
 declare global {
   var _mongoClientPromise: Promise;
+
+  type Sector = {
+    sector: string;
+    sector_id: string;
+    crag_id: string;
+    crag: string;
+    country: string;
+    routes: array[];
+  };
 }
 
 export {};
