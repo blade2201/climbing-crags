cb9ac89ff66c6d42dcf42d8d7df6de6a360fd186
hudsonbay97@gmail.com <hudsonbay97@gmail.com>
2022-08-09 10:28:05 +0200

    Rating test completed

--- tests/components/Rating.spec.tsx
+++ tests/components/Rating.spec.tsx
@@ -1,20 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import { getFontDefinitionFromNetwork } from 'next/dist/server/font-utils';
 import React from 'react';
 import Rating from '../../components/ui/Rating';
-import ReactTestUtils from 'react-dom/test-utils';
 
 describe('Rating', () => {
   it('renders the stars correctly', () => {
     render(<Rating rating={3.5} />);
-
+    const firstStar = screen.getByTestId('star-1');
+    const secondStar = screen.getByTestId('star-2');
     const thirdStar = screen.getByTestId('star-3');
     const fourthStar = screen.getByTestId('star-4');
     const fifthStar = screen.getByTestId('star-5');
 
-    console.log(fourthStar);
-    expect(thirdStar.src).toEqual('/star-full.svg');
-    expect(fourthStar).toHaveAttribute('src', '/star-half.svg');
-    expect(fifthStar).toHaveAttribute('src', '/star-empty.svg');
+    expect(firstStar).toHaveClass('/star-full.svg');
+    expect(secondStar).toHaveClass('/star-full.svg');
+    expect(thirdStar).toHaveClass('/star-full.svg');
+    expect(fourthStar).toHaveClass('/star-half.svg');
+    expect(fifthStar).toHaveClass('/star-empty.svg');
   });
 });
