3ed958ec88297202b81fe2b1d4c358e698782d5e
Matthew D'Agostino <mattdags982@gmail.com>
2022-08-10 11:18:43 +0200

    login test logic

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  chromeWebSecurity: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3000",
+  },
+  env: {
+    googleId: process.env.GOOGLE_ID,
+    googleSecret: process.env.GOOGLE_SECRET,
+    googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   },
 });
